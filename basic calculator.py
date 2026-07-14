9a5614e2647f152f5126230c7ce0e3db6009af63
ajuni-d <ajunik.62@gmail.com>
2026-07-14 18:57:32 +0100

    Add files via upload

---
+++ basic calculator.py
@@ -0,0 +1,25 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /, %,**,//): ")
+
+if operation == "+":
+    result = num1 + num2
+    print(result)
+elif operation == "-":
+    result = num1 - num2
+    print(result)
+elif operation == "*":
+    result = num1 * num2
+    print(result)
+elif operation == "/":
+    result = num1 / num2
+    print(result)
+elif operation == "%":
+    result = num1 % num2
+    print(result)
+elif operation == "**":
+    result = num1 ** num2
+    print(result)
+elif operation == "//":
+    result = num1 // num2
+    print(result)
